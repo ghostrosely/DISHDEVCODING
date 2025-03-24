98d69a0d1a9164015b2e1798e3d9ae62f1a47c41
BingZh <6047656@mborijnland.nl>
2025-03-24 10:13:45 +0100

    added new pages

---
+++ PHP/header.php
@@ -0,0 +1,19 @@
+
+<header>
+
+<br>
+<nav>
+    <a href="index.html">Home </a>
+    <a href="lunch & diner.html">Lunch & Diner </a>
+    <a href="openingstijden & locatie.html">Openingstijden & Locatie </a>
+    <a href="reseveren.html">Reseveren </a>
+    <a href="vacatures.html">Vacatures </a>
+</nav>
+
+<a href="index.html"><img class="profile" src="c:\xampp\htdocs\DISHDEVCODING\IMG\ix_user-profile-filled.png" height="40" width="40"></a>
+
+<article class="logo">
+    <a href="index.html"><img src="c:\xampp\htdocs\DISHDEVCODING\IMG\vegan.png" alt="veganfoodlogo" height="68" width="90"></a>
+</article>
+
+</header>
