06d063f8e991bf351d69768ea3d9d027938eb24c
BingZh <6047656@mborijnland.nl>
2025-03-26 09:32:43 +0100

    changed HTML to php

---
+++ HTML/index.php
@@ -0,0 +1,63 @@
+<!DOCTYPE html>
+<html lang="nl">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+
+    <meta name="description" content="Homepage voor Vegan food">
+    <meta name="keywords" content="Restaurant : Vegan Food, Food, Vegan">
+    <meta name="author" content="Bingyi Zhou">
+
+    <title>Home</title>
+    <link rel="stylesheet" href="stylesheetdish.css">
+</head>
+
+<body>
+ 
+    
+    <header>
+
+        <?php include "PHP/header.php"?>
+
+    </header>
+
+    <section class="banner">
+
+    </section>
+   
+        <main>
+            <img class="homeimg" src="IMG/Veganfoodhome.png" alt="homepic">
+
+                <article class="heading">
+                    <p class="title">Welkom!</p>
+                    <p class="paragraph">Ben jij op zoek naar een culinaire ervaring 
+                        <br> die zowel heerlijk als verantwoord is? 
+                        <br>We zijn verheugd je te mogen verwelkomen 
+                        <br> in ons duurzame en gastvrije vegan restaurant. 
+                        <br>Hier streven we ernaar om je te verassen 
+                        <br> met heerlijke en creatief bereide plantaardige 
+                        <br>gerechten.</p>
+                </article>
+
+                <article id="about">
+                    <p class="title">Over ons</p>
+                    <p class="paragraph">
+                        Wij streven ernaar om niet alleen een plek te bieden voor
+                        <br> vegans,maar voor iedereen die nieuwsgierig is naar de 
+                        <br>veelzijdigheid van plantaardig koken. Kom langs, ontdek 
+                        <br>de smaak van een plantaardige toekomst en deel in onze 
+                        <br>planeet. Kom even kijken wat we allemaal voorbereiden!</p>
+                </article>
+
+                <section class="klikken">
+                    <button type="button" id="reseveren">Reseveer nu!</button>
+                    <button type="button" id="locatie">Locaties & Openingstijden</button>
+                </section>
+        </main>
+ 
+    <footer>
+        <?php include "PHP/footer.php"?>
+    </footer>
+</body>
+</html>
